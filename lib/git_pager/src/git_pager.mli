a1afc63bce37440edc603da383438592ad9fc200
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-05-30 10:21:45 +0200

    Update project headers

--- lib/git_pager/src/git_pager.mli
+++ lib/git_pager/src/git_pager.mli
@@ -1,5 +1,5 @@
 (*_********************************************************************************)
-(*_  Git_pager - Run a Git pager to display diffs and other custom outputs        *)
+(*_  pageantty - Run a pager to display diffs and other outputs in the terminal   *)
 (*_  SPDX-FileCopyrightText: 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
 (*_  SPDX-License-Identifier: MIT                                                 *)
 (*_********************************************************************************)
